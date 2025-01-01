07676354aada8e350c81cff17301ac31a075146f
Gomer Gaufo Jr. <123292825+KiraaDev@users.noreply.github.com>
2025-01-01 22:22:10 +0800

    Add database config

---
+++ database/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$username = "";
+$password = "";
+$database = "school_management";
+
+$conn = new PDO("mysql:host=localhost; dbname=$database", $username, $password);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if($conn){
+	
+} else {
+ die("cant connect to database");
+}
+
+?>
